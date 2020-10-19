c3a8fcfd626765f59a7ebb1bd8adf0228c9e0f87
Saurabh <saurabhguptajpr@yahoo.in>
2020-10-19 15:09:44 +0530

    Updates

---
+++ precache-manifest.6f827e714038fd24c32a964492d4ed9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58928802bfed9a79b5bd75684184f323",
+    "url": "/Todo-react/index.html"
+  },
+  {
+    "revision": "e049959ce144320712c1",
+    "url": "/Todo-react/static/css/main.042a2316.chunk.css"
+  },
+  {
+    "revision": "185b4a4fdf3887ad3735",
+    "url": "/Todo-react/static/js/2.bee72a61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-react/static/js/2.bee72a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e049959ce144320712c1",
+    "url": "/Todo-react/static/js/main.97dcbf1d.chunk.js"
+  },
+  {
+    "revision": "6ed42167067b07f5c269",
+    "url": "/Todo-react/static/js/runtime-main.7548d7c8.js"
+  },
+  {
+    "revision": "5cd352dab085ae0221c21dde8b585d56",
+    "url": "/Todo-react/static/media/background.5cd352da.png"
+  }
+]);
